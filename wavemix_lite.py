fe14a9686a5a3888dcf9996cb57d5a6f962954dd
dodoseung <sok7819@gmail.com>
2022-11-16 17:46:04 +0900

    wavemix

--- wavemix_lite.py
+++ wavemix_lite.py
@@ -1,45 +1,76 @@
-import torch.nn.functional as F
+import torch
 import torch.nn as nn
 
-class WaveMixLite(nn.Module):
-    def __init__(self, block_num=7, channel_dim=16, dropout=0.5):
-        super(WaveMixLite, self).__init__()
-        # Set the number of blocks
-        self.block_num = block_num
 
-        # Set the wave mix lite block
-        self.wavemixlite = nn.ModuleList([WaveMixLiteBlock(channel_dim=channel_dim, dropout=dropout) for _ in range(self.block_num)])
+class twoDDWT(nn.Module):
+    def __init__(self, device):
+        super(twoDDWT, self).__init__()
+        self.n = torch.tensor(2, dtype=torch.float32, device=device)
 
     def forward(self, x):
-        # Wave mix lite blocks
-        for i in range(self.block_num):
-            x = self.wavemixlite[i](x)
-        
-        return x
+        low, high = self.haar_transform_vertical(x)
+        ll, lh = self.haar_transform_horizontal(low)
+        hl, hh = self.haar_transform_horizontal(high)
 
+        return ll, lh, hl, hh
+    
+    def haar_transform_vertical(self, x):
+        low = (x[:, :, ::2, :] + x[:, :, 1::2, :]) / torch.sqrt(self.n)
+        high = (x[:, :, ::2, :] - x[:, :, 1::2, :]) / torch.sqrt(self.n)
+
+        return low, high
+
+    def haar_transform_horizontal(self, x):
+        low = (x[:, :, :, ::2] + x[:, :, :, 1::2]) / torch.sqrt(self.n)
+        high = (x[:, :, :, ::2] - x[:, :, :, 1::2]) / torch.sqrt(self.n)
+        
+        return low, high
 
 class WaveMixLiteBlock(nn.Module):
-    def __init__(self, channel_dim=16, dropout=0.5):
+    def __init__(self, dim_channel=128, mul_factor=2, dropout=0.5, device='cpu'):
         super(WaveMixLiteBlock, self).__init__()
         # Set the channel number
-        self.channel_dim = channel_dim
+        self.dim_channel = dim_channel
 
         # Set the layers
-        self.conv_input = nn.Conv2d(self.channel_dim, self.channel_dim / 4, 1, padding='same')
-        self.twod_dwt = twoDDWT()
-        self.fc = nn.Conv2d(self.channel_dim, self.channel_dim, kernel_size=(1, 1), stride=(1, 1), padding='same')
-        self.conv_trans = nn.ConvTranspose2d(self.channel_dim, self.channel_dim, kernel_size=(4, 4), stride=(2, 2), padding=1)
-        self.batch_norm = nn.BatchNorm2d(self.channel_dim)
+        # Work for reduction the parameters and compuations
+        self.conv_input = nn.Conv2d(self.dim_channel, int(self.dim_channel/4), kernel_size=(1, 1), stride=(1, 1))
+        self.twod_dwt = twoDDWT(device=device)
+
+        # MLP layer (two 1 × 1 convolutional layers separated by a GELU non-linearity)
+        self.fc = nn.Sequential(
+            nn.Conv2d(self.dim_channel, self.dim_channel * mul_factor, kernel_size=(1, 1), stride=(1, 1)),
+            nn.GELU(),
+            nn.Dropout(p=dropout),
+            nn.Conv2d(self.dim_channel * mul_factor, self.dim_channel, kernel_size=(1, 1), stride=(1, 1))
+            )
+
+        # Transposed convolution to reconciliate the image size
+        self.conv_trans = nn.ConvTranspose2d(self.dim_channel, self.dim_channel, kernel_size=(4, 4), stride=(2, 2), padding=(1, 1))
+        
+        # Batch normalization
+        self.batch_norm = nn.BatchNorm2d(self.dim_channel)
 
     def forward(self, x):
         # Set the residual
         residual = x
 
-        # Network
+        # Input conv layer
         x = self.conv_input(x)
-        x = self.twod_dwt(x)
+
+        # 2D-DWT
+        ll, lh, hl, hh = self.twod_dwt(x)
+
+        # Concatenate the output images of 2D-DWT
+        x = torch.cat((ll, lh, hl, hh), 1)
+        
+        # MLP
         x = self.fc(x)
+
+        # Transposed conv layer
         x = self.conv_trans(x)
+
+        # Batch normalization
         x = self.batch_norm(x)
 
         # Residual connection
@@ -48,9 +79,50 @@ class WaveMixLiteBlock(nn.Module):
         return x
 
 
-class twoDDWT(nn.Module):
-    def __init__(self):
-        super(twoDDWT, self).__init__()
-        
+class WaveMixLite(nn.Module):
+    def __init__(self, num_block=8, dim_channel=128, mul_factor=2, dropout=0.5, device='cpu'):
+        super(WaveMixLite, self).__init__()
+        # Set the number of blocks
+        self.num_block = num_block
+
+        # Set the device
+        self.device = device
+
+        # Set the wave mix lite block
+        self.wavemixlite = nn.ModuleList([WaveMixLiteBlock(dim_channel=dim_channel, mul_factor=mul_factor, dropout=dropout, device=device) for _ in range(self.num_block)])
+
     def forward(self, x):
+        # Wave mix lite blocks
+        for i in range(self.num_block):
+            x = self.wavemixlite[i](x)
+        
+        return x
+
+
+class WaveMixLiteImageClassification(nn.Module):
+    def __init__(self, num_class=1000, num_block=8, dim_channel=128, mul_factor=2, dropout=0.5, device='cpu'):
+        super(WaveMixLite, self).__init__()
+        # Set the wave mix lite network
+        self.wavemixlite = WaveMixLite(num_block=num_block, dim_channel=dim_channel, mul_factor=mul_factor, dropout=dropout, device=device)
+
+        # Set the number of classes
+        self.num_class = num_class
+
+        # Set the initial conv layer
+        self.conv = nn.Sequential(
+            nn.Conv2d(3, int(dim_channel / 2), kernel_size=(3, 3), stride=(1, 1), padding='same'),
+            nn.Conv2d(int(dim_channel / 2), dim_channel, kernel_size=(3, 3), stride=(1, 1), padding='same')
+        )
+
+        # Set the MLP head layer
+        self.fc = nn.Linear()
+
+        # Set the global average pooling
+        self.pool = nn.AvgPool1d(1)
+
+        # Set the softmax function
+        self.softmax = nn.Softmax()
+
+    def forward(self, x):
+
         return x
--- wavemix_lite_image_classification.py
+++ wavemix_lite_image_classification.py
@@ -1,3 +1,5 @@
+from wavemix_lite import WaveMixLite
+
 import torch
 import torch.optim as optim
 import torch.nn.functional as F
@@ -7,3 +9,36 @@ from torchvision import datasets, transforms
 
 import matplotlib.pyplot as plt
 
+# Data parameters
+batch_size = 256
+shuffle = True
+drop_last = True
+download = True
+# Training parameters
+epochs = 10
+learning_rate = 0.0002
+# Network parameters
+num_layers = 10
+# Other parameters
+log_period = 50
+
+# Training setting
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
+torch.manual_seed(777)
+if device == 'cuda':
+  torch.cuda.manual_seed_all(777)
+
+# Set the transform
+transform = transforms.Compose([transforms.ToTensor()])
+
+# Set the training data
+data_train = datasets.CIFAR10('~/.pytorch/CIFAR_data/', download=download, train=True, transform=transform)
+loader_train = torch.utils.data.DataLoader(data_train, batch_size=batch_size, shuffle=shuffle, drop_last=drop_last)
+
+# Set the test data
+data_test = datasets.CIFAR10('~/.pytorch/CIFAR_data/', download=download, train=False, transform=transform)
+loader_test = torch.utils.data.DataLoader(data_test, batch_size=batch_size, shuffle=shuffle, drop_last=drop_last)
+
+# Set the model
+model = WaveMixLite(block_num=7, channel_dim=16, mul_factor=2, dropout=0.5, device=device).to(device)
+print(model, device)
