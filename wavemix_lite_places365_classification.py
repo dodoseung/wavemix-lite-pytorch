cc5dd1ac5a24855ae9a488a8caa121d77ffb148a
dodoseung <sok7819@gmail.com>
2022-11-21 19:20:13 +0900

    fix

---
+++ wavemix_lite_places365_classification.py
@@ -0,0 +1,97 @@
+from wavemix_lite import WaveMixLiteImageClassification
+
+import torch
+import torch.optim as optim
+import torch.nn.functional as F
+import torch.nn as nn
+
+from torchvision import datasets, transforms
+
+import matplotlib.pyplot as plt
+
+# Data parameters
+batch_size = 256
+shuffle = True
+drop_last = True
+download = True
+num_workers = 4
+# Model parameters
+num_class=10
+num_block=6
+dim_channel=64
+mul_factor=2
+dropout=0.5
+# Training parameters
+epochs = 100
+lr=1e-3
+betas=(0.9, 0.999)
+eps=1e-8
+weight_decay=1e-2
+# Other parameters
+log_period = 50
+
+# Training setting
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
+torch.manual_seed(777)
+if device == 'cuda':
+  torch.cuda.manual_seed_all(777)
+
+# Set the transform
+transform = transforms.Compose([transforms.ToTensor(),
+                                transforms.RandomResizedCrop(224),
+                                transforms.RandomHorizontalFlip(),
+                                transforms.Normalize(mean=[0.485, 0.456, 0.406],
+                                                     std=[0.229, 0.224, 0.225])])
+
+# Set the training data
+train_data = datasets.Places365('~/.pytorch/PLACES365_data/', split="train-standard", small=True, download=download, transform=transform)
+train_loader = torch.utils.data.DataLoader(train_data, batch_size=batch_size, shuffle=shuffle, num_workers=num_workers, drop_last=drop_last)
+
+# Set the test data
+test_data = datasets.Places365('~/.pytorch/PLACES365_data/', split="val", small=True, download=download, transform=transform)
+test_loader = torch.utils.data.DataLoader(test_data, batch_size=batch_size, shuffle=shuffle, num_workers=num_workers, drop_last=drop_last)
+
+# Set the model
+model = WaveMixLiteImageClassification(num_class=num_class,
+                                       num_block=num_block,
+                                       dim_channel=dim_channel,
+                                       mul_factor=mul_factor,
+                                       dropout=dropout,
+                                       device=device).to(device)
+print(model, device)
+
+# Set the criterion and optimizer
+criterion = nn.CrossEntropyLoss()
+optimizer = optim.AdamW(model.parameters(),
+                        lr=lr,
+                        betas=betas,
+                        eps=eps,
+                        weight_decay=weight_decay)
+
+# Training
+for epoch in range(epochs):  # loop over the dataset multiple times
+    running_loss = 0.0
+    for i, data in enumerate(train_loader, 0):
+        # get the inputs; data is a list of [inputs, labels]
+        inputs, labels = data
+        
+        # Transfer data to device
+        inputs = inputs.to(device)
+        labels = labels.to(device)
+
+        # Model inference
+        outputs = model(inputs)
+        
+        # Training
+        optimizer.zero_grad()
+        loss = criterion(outputs, labels)
+        loss.backward()
+        optimizer.step()
+
+        # print statistics
+        running_loss += loss.item()
+        if i % log_period == 0 and i != 0:
+            print(f'[{epoch}, {i:5d}] loss: {running_loss / log_period:.3f}')
+            running_loss = 0.0
+
+print('Finished Training')
